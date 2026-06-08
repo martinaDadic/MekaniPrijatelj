e09106cefe12ea80386bbf7f5b1d3014ca0ba358
Martina Dadić <md54715@fer.hr>
2026-06-08 03:35:49 +0200

    ispravak

--- src/main.js
+++ src/main.js
@@ -14,7 +14,8 @@ app.use(
         domain: import.meta.env.VITE_AUTH0_DOMAIN,
         clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
         authorizationParams: {
-            redirect_uri: window.location.origin
+            redirect_uri: window.location.origin,
+            audience: "https://api.mekaniprijatelj.com"
         }
     })
 )
