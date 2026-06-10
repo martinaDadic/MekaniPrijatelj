a1172e245a0ffa8e12690e4cad5e041af1d0560f
Antonio Šulenta <as55007@fer.hr>
2026-06-10 14:27:27 +0200

    Update 2

--- src/homeAssistant.js
+++ src/homeAssistant.js
@@ -1,5 +1,5 @@
 const HA_TOKEN = import.meta.env.VITE_HA_TOKEN;
-const HA_BASE = 'https://myndcicsudpm1rggzebe2oy5qtkskvtx.ui.nabu.casa/';
+const HA_BASE = 'https://myndcicsudpm1rggzebe2oy5qtkskvtx.ui.nabu.casa';
 
 const headers = {
   Authorization: `Bearer ${HA_TOKEN}`,
